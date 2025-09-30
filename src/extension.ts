a33370b289dd914a8b812e1df67bb5fe923e954d
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2025-09-30 12:51:13 +0200

    Second step

---
+++ src/extension.ts
@@ -0,0 +1,7 @@
+export function activate(context: any) {
+    console.log('VS Code Memory Manager extension activated');
+}
+
+export function deactivate() {
+    console.log('VS Code Memory Manager extension deactivated');
+}
